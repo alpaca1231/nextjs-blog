96ab19c19b9b81e0aec4999adc84abbfff07fb2a
alpaca1231 <kazuki.amatatsu.fd@gmail.com>
2023-12-06 22:07:26 +0900

    wip

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,11 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
   return {
     props: {
-      allPostsData: allPostsData,
+      allPostsData,
     },
   }
 }
